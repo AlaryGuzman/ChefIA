fc92aa13673ec4bc74c40c3dd9721d9943c18261
Alexander <alexanderjuarez0055@gmail.com>
2026-07-14 22:43:45 -0600

    Configurar ruta para crear admins

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -23,6 +23,7 @@ class UserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users,email',
             'password' => 'required|string|min:6',
+            'role' => 'required|string|in:admin,usuario',
         ]);
 
         $validated['password'] = Hash::make($validated['password']);
